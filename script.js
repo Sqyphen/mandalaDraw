6e4a19ff6fbb7c9779268513c6e4cb1c5c95d9fe
Tobias <tobias.fieldhouse@wearesocial.net>
2017-07-09 22:49:23 +0100

    reduces number of specified array
    stops using an array for each X/Y co-ordinate and instead draws each point in relation to one set of 'origin' points

--- script.js
+++ script.js
@@ -1,41 +1,23 @@
-
-var canvas = document.getElementById('canvas');
-var context = canvas.getContext("2d");
-var paint = false;
-
-var clickDrag = new Array();
-
-var clickX = new Array();
-var clickY = new Array();
-var clickX2 = new Array();
-var clickY2 = new Array();
-var clickX3 = new Array();
-var clickY3 = new Array();
-var clickX4 = new Array();
-var clickY4 = new Array();
-var clickX5 = new Array();
-var clickY5 = new Array();
-
-var clearButton = document.getElementById("clearButton");
-
+var canvas = document.getElementById('canvas'),
+	context = canvas.getContext("2d"),
+	clearButton = document.getElementById("clearButton"),
+	paint = false,
+	clickX = [],
+	clickY = [],
+	clickDrag = [],
+	pointTransformations = [ 0, -72, -144, -216, -284 ];
 
 function init()
 {
 	clearButton.addEventListener("click", clearDrawing);
-
 	context.save();
 	context.translate((canvas.height/2), (canvas.width/2));
 }
 
 canvas.onmousedown = function (e)
 {
-	var mouseX = e.pageX - this.offsetLeft;
-	var mouseY = e.pageY - this.offsetTop;
-
 	paint = true;
-
 	addClick(e.pageX - this.offsetLeft, e.pageY - this.offsetTop);
-	
 	redraw();
 };
 
@@ -60,43 +42,8 @@ canvas.onmouseleave = function(e)
 
 function addClick(x, y, dragging)
 {
-	var offsetX = canvas.width/2;
-	var offsetY = canvas.height/2;
-
-	var newX = x-offsetX;
-	var newY = y-offsetY;
-
-	clickX.push(newX);
-	clickY.push(newY);
-
-	var rotated
-
-	rotated = rotate(0, 0, newX, newY, -72);
-	clickX2.push(rotated[0]);
-	clickY2.push(rotated[1]);
-
-	rotated = rotate(0, 0, newX, newY, -144);
-	clickX3.push(rotated[0]);
-	clickY3.push(rotated[1]);
-
-	rotated = rotate(0, 0, newX, newY, -216);
-	clickX4.push(rotated[0]);
-	clickY4.push(rotated[1]);
-
-	rotated = rotate(0, 0, newX, newY, -284);
-	clickX5.push(rotated[0]);
-	clickY5.push(rotated[1]);
-
-
-	//clickX2.push(-newX+canvas.width);
-	//clickY2.push(newY);
-
-	//clickX3.push(-newX+canvas.width);
-	//clickY3.push(-newY+canvas.height);
-
-	//clickX4.push(newX);
-	//clickY4.push(-newY+canvas.height);
-
+	clickX.push(x-(canvas.width/2));
+	clickY.push(y-(canvas.height/2));
 	clickDrag.push(dragging);
 }
 
@@ -115,44 +62,55 @@ function redraw()
 	context.strokeStyle = "#ffffff";
 	context.lineJoin = "round";
 	context.lineWidth = 3;
-			
-	drawLines(clickX, clickY);
-	drawLines(clickX2, clickY2);
-	drawLines(clickX3, clickY3);
-	drawLines(clickX4, clickY4);
-	drawLines(clickX5, clickY5);
+	drawLines();
 }
 
-function drawLines( pointsX, pointsY, rotation )
+function drawLines()
 {
-	var lenX = pointsX.length;
-	var lenY = pointsX.length;
+	var clickLength = clickX.length;
+	var previousX = clickX[clickLength-2];
+	var previousY = clickY[clickLength-2];
+	var nextX = clickX[clickLength-1];
+	var nextY = clickY[clickLength-1];
+
+	pointTransformations.forEach(function(angle) {
+		var points = getDrawPoints(previousX, previousY, nextX, nextY, angle);
+		drawLine(points.pX, points.pY, points.nX, points.nY);
+		drawLine(points.pX, -points.pY, points.nX, -points.nY);
+	});
+}
+
+function drawLine(pX, pY, nX, nY)
+{
 	context.beginPath();
-	context.moveTo(pointsX[lenX-2], pointsY[lenY-2]);
-	context.lineTo(pointsX[lenX-1], pointsY[lenY-1]);
+	context.moveTo(pX, pY);
+	context.lineTo(nX, nY);
 	context.closePath();
 	context.stroke();
 }
 
-function resetArrays()
+function getDrawPoints(previousX, previousY, nextX, nextY, angle)
 {
-	clickX = new Array();
-	clickY = new Array();
-	clickX2 = new Array();
-	clickY2 = new Array();
-	clickX3 = new Array();
-	clickY3 = new Array();
-	clickX4 = new Array();
-	clickY4 = new Array();
-	clickX5 = new Array();
-	clickY5 = new Array();
+	var previousRotated = rotate(0, 0, previousX, previousY, angle);
+	var nextRotated = rotate(0, 0, nextX, nextY, angle);
+	var points = {
+		'pX' : previousRotated[0],
+		'pY' : previousRotated[1],
+		'nX' : nextRotated[0],
+		'nY' : nextRotated[1],
+	};
+	return points;
+}
+
+function undoDrawing() {
+
 }
 
 function clearDrawing(e) {
 	e.preventDefault();
-	resetArrays();
-	context.clearRect(0, 0, canvas.width, canvas.height);
+	clickX = [];
+	clickY = [];
+	context.clearRect(-canvas.width/2, -canvas.height/2, canvas.width, canvas.height);
 }
 
 init();
-
