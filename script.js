1311c5896e1f87c611a0286a138db25a00b3ada9
Tobias Fieldhouse <tobias.fieldhouse@wearesocial.net>
2017-07-26 09:51:56 +0100

    almost there!

--- script.js
+++ script.js
@@ -34,11 +34,12 @@ canvas.onmousedown = function (e)
 {
 	paint = true;
 	addClick(e.pageX - this.offsetLeft, e.pageY - this.offsetTop);
-	redraw();
+	//redraw();
 };
 
 canvas.touchstart = function (e)
 {
+	console.log("touchstart");
 	paint = true;
 	addClick(e.pageX - this.offsetLeft, e.pageY - this.offsetTop);
 	redraw();
@@ -55,6 +56,7 @@ canvas.onmousemove = function(e)
 
 canvas.touchmove = function(e)
 {
+	console.log("touchmove");
 	if(paint)
 	{
 		addClick(e.pageX - this.offsetLeft, e.pageY - this.offsetTop, true);
@@ -69,6 +71,7 @@ canvas.onmouseup = function(e)
 
 canvas.touchend = function(e)
 {
+	console.log("touchend");
 	paint = false;
 };
 
@@ -80,17 +83,25 @@ canvas.onmouseleave = function(e)
 function addClick(x, y, dragging)
 {
 	var offsetX = x - (canvas.width/2);
-	var offsetY = y - (canvas.height/2)
+	var offsetY = y - (canvas.height/2);
+
+	/*
+	var previousX = clickX[clickX.length-1];
+	var previousY = clickY[clickY.length-1];
+
+
+	if((previousX-offsetX) <= 1 && (previousX-offsetX) >= -1){
+		clickX.pop();
+	}
+
+	if((previousY-offsetY) <= 1 && (previousY-offsetY) >= -1){
+		clickY.pop();
+	}
+	*/
 
 	clickX.push(offsetX);
 	clickY.push(offsetY);
 	clickDrag.push(dragging);
-
-	context.rotate(90*Math.PI/180);
-	context.rect(offsetX,offsetY,3,3);
-	context.fillStyle = "#fff";
-	context.fill();
-	context.stroke();
 
 }
 
@@ -114,7 +125,8 @@ function redraw()
 	context.shadowOffsetX = 0;
 	context.shadowOffsetY = 0;
 
-	drawLines();
+	//drawLines();
+	drawLinesRedraw();
 }
 
 function drawLines()
@@ -123,9 +135,7 @@ function drawLines()
 	var previousX = clickX[clickLength-2];
 	var previousY = clickY[clickLength-2];
 	var nextX = clickX[clickLength-1];
-	var nextY = clickY[clickLength-1];
-
-	//context.clearRect(-canvas.width/2, -canvas.height/2, canvas.width, canvas.height);
+	var nextY = clickY[clickLength-1]; 
 
 	pointTransformations.forEach(function(angle) {
 		var points = getDrawPoints(previousX, previousY, nextX, nextY, angle);
@@ -134,11 +144,60 @@ function drawLines()
 	});
 }
 
+function drawLinesRedraw()
+{
+	context.clearRect(-canvas.width/2, -canvas.height/2, canvas.width, canvas.height);
+
+	pointTransformations.forEach(function(angle)
+	{
+		for(var i=0; i < clickX.length; i++)
+		{
+			if(i % 2 == 0)
+			{
+				slicedX = clickX.slice(Math.max(clickX.length - 55, 1));
+				slicedY = clickY.slice(Math.max(clickY.length - 55, 1));
+
+				var originX = slicedX[i-2];
+				var originY = slicedY[i-2];
+				var previousX = slicedX[i-1];
+				var previousY = slicedY[i-1];
+				var nextX = slicedX[i];
+				var nextY = slicedY[i]; 
+				var points = getDrawCurvePoints(originX, originY, previousX, previousY, nextX, nextY, angle);
+				drawCurve(points.pXorigin, points.pYorigin, points.pX, points.pY, points.nX, points.nY);
+				drawCurve(points.pXorigin, -points.pYorigin, points.pX, -points.pY, points.nX, -points.nY);
+			}
+		}
+
+		var points = getDrawPoints(0, 0, clickX[clickX.length-1], clickY[clickY.length-1], angle);
+		context.rect(points.nX, points.nY,3,3);
+		context.fillStyle = "#fff";
+		context.fill();
+		context.stroke();
+
+		context.rect(points.nX, -points.nY,3,3);
+		context.fillStyle = "#fff";
+		context.fill();
+		context.stroke();
+
+	});
+
+}
+
 function drawLine(pX, pY, nX, nY)
 {
 	context.beginPath();
 	context.moveTo(pX, pY);
-	context.lineTo(nX, nY);
+	context.quadraticCurveTo(pX, pY, nX, nY);
+	context.closePath();
+	context.stroke();
+}
+
+function drawCurve(pX0, pY0, pX, pY, nX, nY)
+{
+	context.beginPath();
+	context.moveTo(pX0, pY0);
+	context.quadraticCurveTo(pX, pY, nX, nY);
 	context.closePath();
 	context.stroke();
 }
@@ -148,6 +207,22 @@ function getDrawPoints(previousX, previousY, nextX, nextY, angle)
 	var previousRotated = rotate(0, 0, previousX, previousY, angle);
 	var nextRotated = rotate(0, 0, nextX, nextY, angle);
 	var points = {
+		'pX' : previousRotated[0],
+		'pY' : previousRotated[1],
+		'nX' : nextRotated[0],
+		'nY' : nextRotated[1],
+	};
+	return points;
+}
+
+function getDrawCurvePoints(originX, originY, previousX, previousY, nextX, nextY, angle)
+{
+	var originRotated = rotate(0, 0, originX, originY, angle);
+	var previousRotated = rotate(0, 0, previousX, previousY, angle);
+	var nextRotated = rotate(0, 0, nextX, nextY, angle);
+	var points = {
+		'pXorigin' : originRotated[0],
+		'pYorigin' : originRotated[1],
 		'pX' : previousRotated[0],
 		'pY' : previousRotated[1],
 		'nX' : nextRotated[0],
